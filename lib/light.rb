3d6d09bd1ce8619c333446af99cc0dd0fe8d4623
reppard <reppardwalker@gmail.com>
2014-09-12 17:00:12 -0400

    cleanup some things

---
+++ lib/light.rb
@@ -0,0 +1,35 @@
+require 'pi_piper'
+include PiPiper
+
+class Light
+  PINS = {
+    "green" => 4,
+    "red" => 17,
+    "yellow" => 27
+  }
+
+  def initialize(color)
+    @color = color
+    @pin = PiPiper::Pin.new(:pin => PINS[color], :direction => :out)
+  end
+
+  def on
+    @pin.on
+  end
+
+  def off
+    @pin.off
+  end
+
+  def flash(times)
+    Process.detach(fork{
+    times.times do |i|
+      puts "DEBUG: #{i}"
+      on
+      sleep 0.05
+      off
+      sleep 0.05
+    end
+    })
+  end
+end
--- lights.rb
+++ lights.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
-require 'pi_piper'
-include PiPiper
+require_relative 'lib/light.rb'
 
 set :port, 80
 set :bind, '0.0.0.0'
@@ -18,46 +17,11 @@ post '/flash' do
   redirect '/'
 end
 
-get '/green_off' do
-  green = PiPiper::Pin.new(:pin => 4, :direction => :out)
-  green.off
-end
-
 get '/disco' do
-  lights = [Light.new("green"),Light.new("red"),Light.new("yellow")]
+  lights = [ Light.new("green"),
+             Light.new("red"),
+             Light.new("yellow")]
+
   lights.each{|light| light.flash(100)}
   redirect '/'
-end
-
-class Light
-  PINS = {
-    "green" => 4,
-    "red" => 17,
-    "yellow" => 27
-  }
-
-  def initialize(color)
-    @color = color
-    @pin = PiPiper::Pin.new(:pin => PINS[color], :direction => :out)
-  end
-
-  def on
-    @pin.on
-  end
-
-  def off
-    @pin.off
-  end
-
-  def flash(times)
-    Process.detach(fork{
-    times.times do |i|
-      puts "DEBUG: #{i}"
-      on
-      sleep 0.05
-      off
-      sleep 0.05
-    end
-    })
-  end
 end
